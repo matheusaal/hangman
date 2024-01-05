cb960f90eba0c70ccbcefd13ebfb05e0a4c4f9fe
matheusaal <matheus.avila31@gmail.com>
2024-01-05 12:10:25 -0300

    improve documentation

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ def game():
     secretWordClass = "Food"
     wrongTries = []
     oldTries = []
+    
     mask = support_functions.createMask(secretWord)
 
     print("\nClass:", secretWordClass, "\n")
@@ -15,6 +16,7 @@ def game():
 
         support_functions.printList(mask)
 
+        # Verify defeat
         if (wrongTries == 6):
 
             communication.loserMessage()
@@ -24,29 +26,31 @@ def game():
 
         currentTry = input("Which letter do you choose? ")
 
+        # Right try logic
         if (currentTry not in oldTries and currentTry in secretWord):
 
             print("\n\nWell done! Right try. \n")
-            indexLetter = support_functions.findLettersIndex(secretWord, currentTry)
+            indexLetter = support_functions.findLettersIndex(secretWord, currentTry) # Find all index of a letter in the secret word
             
             for i in indexLetter:
 
-                mask[i] = currentTry
+                mask[i] = currentTry # Change the "_" from the mask to the correct letter
     
-            oldTries.append(currentTry)
+            oldTries.append(currentTry) # Add the current try to a list containing all old tries
             
-
+        # Wrong try logic
         elif (currentTry not in oldTries and currentTry not in secretWord):
 
             print("\nOoh! What a shame, your try is wrong.\n")
 
         
-            oldTries.append(currentTry)
-            wrongTries.append(currentTry)
+            oldTries.append(currentTry) # Add the current try to a list containing all old tries
+            wrongTries.append(currentTry) #  Add the current try to a list containing just the wrong tries (to show them later)
 
-            leftTries = 6 - len(wrongTries)
+            leftTries = 6 - len(wrongTries) # Calculate the left tries using the size of the wrong tries list
+            wrongTriesQuantity = len(wrongTries) # Calculate the wrong tries amount using the size of the wrong tries list
 
-            communication.hangmanDrawing(len(wrongTries))
+            communication.hangmanDrawing(wrongTriesQuantity) # Draws a hangman using wrong tries amount as a parameter
 
             print("You have", leftTries, "tries left.\n")
             print("Old tries: ", end="")
@@ -59,6 +63,7 @@ def game():
 
             print("\nYour attempt is invalid! Please, try again.")
 
+        # Verify victory
         if (mask == support_functions.convertStringToList(secretWord)):
 
             communication.winnerMessage()
